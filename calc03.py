7c15c404cb952dac31590f38229cd6c762b3a09c
1115342730 <1115342730@qq.com>
2017-10-21 20:43:28 +0800

    update

--- calc03.py
+++ calc03.py
@@ -28,7 +28,7 @@ class Args():
 	@property
 	def _path_configfile(self):
 #		return self._value_after_option('-c')
-		return '/home/shiyanlou/c.cfg'
+		return '/home/shiyanlou/c.txt'
 
 	@property
 	def _path_userdatafile(self):
@@ -38,27 +38,27 @@ class Args():
 	def _path_outputfile(self):
 #		return self._value_after_option('-o')
 		return '/home/shiyanlou/o.csv'
+
 args = Args()
 
-class Config():
+class Config(object):
 	def __init__(self):
 		self.config = self._read_configfile()
-
-
 
 	def _read_configfile(self):
 
 		configfile = args._path_configfile
-		self.config =dict()
+		config =dict()
 		with open(configfile) as file:
 			for line in file.readlines():
 				key,value = line.strip().split('=')
 				try:
-					self.config[key] = float(value)
+					config[key] = float(value)
 				except:
 					print(line)
 					print('configfile have error')
 					exit()
+		return config
 
 	def _get_config(self,key):
 		try:
@@ -67,21 +67,25 @@ class Config():
 			print(key,'no exist')
 			exit()
 
-	def _JishuL(self):
-		return(_get_config('JiShuL'))
+	@property
+	def _JiShuL(self):
+		return(self._get_config('JiShuL'))
 
-	def _JishuH(self):
-		return(_get_config('JiShuH'))	
+	@property
+	def _JiShuH(self):
+		return(self._get_config('JiShuH'))
 
+	@property
 	def _rate(self):
-		return sum([
-			_get_config('YangLao'),
-			_get_config('YiLiao'),
-			_get_config('ShiYe'),
-			_get_config('GongShang'),
-			_get_config('ShengYu'),
-			_get_config('GongJiJin'),
-			])
+		s = sum([
+			self._get_config('YangLao'),
+			self._get_config('YiLiao'),
+			self._get_config('ShiYe'),
+			self._get_config('GongShang'),
+			self._get_config('ShengYu'),
+			self._get_config('GongJiJin'),
+			])	
+		return s	
 
 
 config = Config()
@@ -91,11 +95,10 @@ config = Config()
 
 class UserData():
 	def __init__(self):
-		self.userdate = self._read_userdatafile()
+#		self.userdate = self._read_userdatafile()
 
 
-	def _read_userdatafile(self):
-		self.userdata  = []
+#	def _read_userdatafile(self):
 		userdatafile = args._path_userdatafile
 		userdata = []
 		with open(userdatafile) as file:
@@ -106,22 +109,16 @@ class UserData():
 				except:
 					print("userdatafile have error")
 					exit()
-			userdata.append([name,salary])
-		return userdata
-
-	def __iter__(self):
-		return iter(self.userdata)
-
-
-class TaxCalc():
-	def __init__(self,userdata):
+				userdata.append((name,salary))
+#		return userdata
 		self.userdata = userdata
 
 
-	@staticmethod
+	
 	def calc_insurance_money(self,salary):
 		rate = config._rate
 		insurance_money = 0
+
 		if salary < config._JiShuL:
 			insurance_money = 2193.0 * rate
 		elif salary > config._JiShuH:
@@ -130,7 +127,7 @@ class TaxCalc():
 			insurance_money = salary * rate	
 		return insurance_money
 
-	@classmethod
+
 	def calc_tax_and_remain(self,salary):
 		amount = salary * (1 - config._rate) - 3500
 		tax = 0
@@ -151,31 +148,35 @@ class TaxCalc():
 		else:
 		 	tax = (amount * 0.450) -13505
 
-		remain  = salary - calc_insurance_money(salary) - tax
+		remain  = salary - self.calc_insurance_money(salary) - tax
 	
-		return '{:f}'.format(tax),'{:f}'.format(remain)
+		return '{:.2f}'.format(tax),'{:.2f}'.format(remain)
+
+#	@property
+#	def calc_for_all_userdata(self):
 
 
-	def calc_for_all_userdata(self):
+	def _export(self):
 		result = []
-		for name,salary in self.userdata:
-			data = [name,salary]
-			insurance_money = calc_insurance_money(salary)
-			tax,remain = calc_tax_and_remain(salary)
+		for i in self.userdata:
+			name = i[0]
+			salary =i[1]
+			data  = [name, salary]
+			insurance_money = '{:.2f}'.format(self.calc_insurance_money(salary))
+			tax , remain = self.calc_tax_and_remain(salary)
 			data += [insurance_money,tax,remain]
-			result.append(data)
-		return result
 
-	def export(self,default = 'csv'):
-		result = self.calc_for_all_userdata()
+			result.append(data)
+
+
 		with open(args._path_outputfile,'w') as file:
 			writer = csv.writer(file)
 			writer.writerows(result)
 
 
-
-d = TaxCalc(UserData())
-d.export()
+if __name__ == '__main__':
+	d = UserData()
+	d._export()
 
 
 '''
